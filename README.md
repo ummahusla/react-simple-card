97fd3dbf5bc2c680799085b1734322f63bbe2c77
Edvins Antonovs <ummahusla@gmail.com>
2017-09-14 16:01:30 +0100

    :memo: Minor typos

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ npm install --save react-simple-card
 
 ## Usage
 
-```
+```js
 import React from "react";
 import { render } from "react-dom";
 import { Card, CardHeader, CardBody, CardFooter } from "react-simple-card";
--- __tests__/Card.js
+++ __tests__/Card.js
@@ -3,7 +3,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
-import renderer from "react-test-renderer";
 import Card from "../src/components/Card";
 
 it("renders Card correctly", () => {
