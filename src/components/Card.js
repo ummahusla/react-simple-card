abced25f51c981b47100fcbe77ad181f15d027ca
Edvins Antonovs <ummahusla@gmail.com>
2017-09-14 20:07:22 +0100

    Added basic styles for card comp

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,18 @@ type CardProps = {
 	className?: string
 };
 
+const styles = {
+	width: "100%",
+  display: "block",
+	position: "relative",
+  backgroundColor: "#fff",
+	borderRadius: "4px",
+	marginBottom: "10px",
+	border: "1px solid #ccc"
+}
+
 const Card = ({ className = "", children }: CardProps) => {
-	return <div className={`card ${className}`}>{children}</div>;
+	return <div className={`card ${className}`} style={styles}>{children}</div>;
 };
 
 export default Card;
