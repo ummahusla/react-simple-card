545d878f17e94d3a64ca0abf2ca1168933de35c0
Edvins Antonovs <ummahusla@gmail.com>
2017-09-17 20:40:38 +0100

    Exporting the new component

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as Card } from "./components/Card";
 export { default as CardHeader } from "./components/CardHeader";
 export { default as CardBody } from "./components/CardBody";
 export { default as CardFooter } from "./components/CardFooter";
+export { default as ImageHeader } from "./components/ImageHeader";
