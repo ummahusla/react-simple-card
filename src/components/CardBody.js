510e058bae4109e76f6fa6079fbe6bf4c54246ac
Stan Tsouvallas <stsou@stsou-mbp.dolby.net>
2017-10-01 18:12:03 +1100

    Add linter packages (airbnb configuration). Lint codebase. Update test snaposhots.

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -1,22 +1,22 @@
 // @flow
 
-import React from "react";
+import React from 'react';
 
 type CardBodyProps = {
-	children: any,
-	style?: any,
-	className?: string
+ children: any,
+ style?: any,
+ className?: string
 };
 
 const defaultStyles = {
-	padding: "18px"
+  padding: '18px',
 };
 
-export default ({ className = "", style, children }: CardBodyProps) => {
-	const styles = { ...defaultStyles, ...style };
-	return (
-		<div className={`card__body ${className}`} style={styles}>
-			{children}
-		</div>
-	);
+export default ({ className = '', style, children }: CardBodyProps) => {
+  const styles = { ...defaultStyles, ...style };
+  return (
+    <div className={`card__body ${className}`} style={styles}>
+      {children}
+    </div>
+  );
 };
--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -1,25 +1,25 @@
 // @flow
 
-import React from "react";
+import React from 'react';
 
 type CardFooterProps = {
-	children: any,
-	style?: any
+ children: any,
+ style?: any
 };
 
 const defaultStyles = {
-	display: "flex",
-	alignItems: "center",
-	padding: "10px 18px",
-	borderTop: "1px solid #ccc",
-	marginTop: "0"
+  display: 'flex',
+  alignItems: 'center',
+  padding: '10px 18px',
+  borderTop: '1px solid #ccc',
+  marginTop: '0',
 };
 
 export default ({ style, children }: CardFooterProps) => {
-	const styles = { ...defaultStyles, ...style };
-	return (
-		<div className="card__footer" style={styles}>
-			{children}
-		</div>
-	);
+  const styles = { ...defaultStyles, ...style };
+  return (
+    <div className="card__footer" style={styles}>
+      {children}
+    </div>
+  );
 };
