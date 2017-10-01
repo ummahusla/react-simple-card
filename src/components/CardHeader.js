1917ebf26d9044e69daeb2641fe2acb373d5edc1
Stan Tsouvallas <stsou@stsou-mbp.dolby.net>
2017-10-02 10:44:58 +1100

    change linter to tabs and double quotes

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,25 +1,25 @@
 // @flow
 
-import React from 'react';
+import React from "react";
 
 type CardHeaderProps = {
- children: any,
- style?: any
+	children: any,
+	style?: any
 };
 
 const defaultStyles = {
-  display: 'flex',
-  alignItems: 'center',
-  padding: '10px 18px',
-  borderBottom: '1px solid #ccc',
-  marginBottom: '0',
+	display: "flex",
+	alignItems: "center",
+	padding: "10px 18px",
+	borderBottom: "1px solid #ccc",
+	marginBottom: "0",
 };
 
 export default ({ style, children }: CardHeaderProps) => {
-  const styles = { ...defaultStyles, ...style };
-  return (
-    <div className="card__header" style={styles}>
-      {children}
-    </div>
-  );
+	const styles = { ...defaultStyles, ...style };
+	return (
+		<div className="card__header" style={styles}>
+			{children}
+		</div>
+	);
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
 
-export { default as Card } from './components/Card';
-export { default as CardHeader } from './components/CardHeader';
-export { default as CardBody } from './components/CardBody';
-export { default as CardFooter } from './components/CardFooter';
-export { default as ImageHeader } from './components/ImageHeader';
+export { default as Card } from "./components/Card";
+export { default as CardHeader } from "./components/CardHeader";
+export { default as CardBody } from "./components/CardBody";
+export { default as CardFooter } from "./components/CardFooter";
+export { default as ImageHeader } from "./components/ImageHeader";
