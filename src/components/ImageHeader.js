4d97a955ee3508246287d6843c5b3c8cfebb259a
Edvins Antonovs <ummahusla@gmail.com>
2017-11-12 12:19:55 +0000

    Updated <ImageHeader> added optional alt prop

--- src/components/ImageHeader.js
+++ src/components/ImageHeader.js
@@ -3,30 +3,31 @@
 import React from "react";
 
 type ImageHeaderProps = {
- imageSrc: string,
- style?: any
+	imageSrc: string,
+	style?: any,
+	alt?: string
 };
 
 const defaultStyles = {
-  position: "relative"
+	position: "relative"
 };
 
 const defaultImageStyles = {
-  display: "block",
-  width: "100%",
-  borderRadius: "2px 2px 0 0",
-  position: "relative",
-  left: "0",
-  right: "0",
-  top: "0",
-  bottom: "0"
+	display: "block",
+	width: "100%",
+	borderRadius: "2px 2px 0 0",
+	position: "relative",
+	left: "0",
+	right: "0",
+	top: "0",
+	bottom: "0"
 };
 
-export default ({ style, imageSrc }: ImageHeaderProps) => {
-  const styles = { ...defaultStyles, ...style };
-  return (
-    <div className="card__header--image" style={styles}>
-      <img src={imageSrc} style={defaultImageStyles} alt="" />
-    </div>
-  );
+export default ({ style, imageSrc, alt = "" }: ImageHeaderProps) => {
+	const styles = { ...defaultStyles, ...style };
+	return (
+		<div className="card__header--image" style={styles}>
+			<img src={imageSrc} style={defaultImageStyles} alt={alt} />
+		</div>
+	);
 };
