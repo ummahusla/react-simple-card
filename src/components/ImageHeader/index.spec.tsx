0f91c6ae1fed7257b3b4eb6a1ab98c2a1942b259
Edvins Antonovs <ummahusla@gmail.com>
2019-10-24 08:34:47 +0100

    Added a unit test to cover the few new params

--- src/components/ImageHeader/index.spec.tsx
+++ src/components/ImageHeader/index.spec.tsx
@@ -5,6 +5,20 @@ import toJson from 'enzyme-to-json';
 import ImageHeader from './';
 
 it('renders ImageHeader correctly', () => {
-    const wrapper = shallow(<ImageHeader imageSrc="http://via.placeholder.com/350x200" />);
+    const wrapper = shallow(
+        <ImageHeader alt="Alt text for the image" imageSrc="http://via.placeholder.com/350x200" />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('renders ImageHeader correctly with className and extra styles', () => {
+    const wrapper = shallow(
+        <ImageHeader
+            className="image-wrapper"
+            style={{ padding: '3px' }}
+            alt="Alt text for the image"
+            imageSrc="http://via.placeholder.com/350x200"
+        />
+    );
     expect(toJson(wrapper)).toMatchSnapshot();
 });
